0b8692e9c5f7af6d5fd2a3b5b3bee10f2a2b494a
Shayna Patel <shaynapatel@Shaynas-MacBook-Pro.local>
2019-02-17 22:48:53 -0600

    basic requirements dones

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Shayna Patel on 2/17/19.
+//  Copyright © 2019 shaynahpatel@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
